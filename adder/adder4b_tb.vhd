e08fe5d1f3e74e1571551c5e9a9e77e6078c7fd4
Martin Maly <maly@maly.cz>
2020-05-22 12:55:38 +0200

    entity adder

--- adder/adder4b_tb.vhd
+++ adder/adder4b_tb.vhd
@@ -1,81 +1,80 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-use IEEE.NUMERIC_STD.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+USE work.bookUtility.ALL; -- for toString
 
-entity adder4b_tb is
-end;
+ENTITY adder4b_tb IS
+END;
 
-architecture bench of adder4b_tb is
+ARCHITECTURE bench OF adder4b_tb IS
+  COMPONENT adder4B IS
+    PORT (
+      A, B : IN std_logic_vector (3 DOWNTO 0);
+      Cin : IN std_logic;
+      Q : OUT std_logic_vector (3 DOWNTO 0);
+      Cout : OUT std_logic);
+  END COMPONENT;
 
-    
-component adder4B is
-  port (A, B: in std_logic_vector (3 downto 0);
-  Cin: in std_logic;
-  Q: out std_logic_vector (3 downto 0);
-  Cout: out std_logic);
-end component;
+  SIGNAL tA, tB, tQ : std_logic_vector (3 DOWNTO 0);
+  SIGNAL tC, tCout : STD_LOGIC;
 
-  signal tA,tB, tQ:std_logic_vector (3 downto 0);
-  signal tC,tCout: STD_LOGIC;
+BEGIN
+  testing : PROCESS
 
-begin
+    PROCEDURE vypis IS
+    BEGIN
+      REPORT toString(tA) & " + " & toString(tB) & " + " & std_logic'image(tC) &
+        " = " & std_logic'image(tCout) & toString(tQ);
+    END PROCEDURE;
 
+  BEGIN
+    tC <= '0';
+    tA <= "0000";
+    tB <= "0000";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '0' AND tQ = "0000" REPORT "0+0+0 failed" SEVERITY failure;
+    tA <= "0001";
+    tB <= "0000";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '0' AND tQ = "0001" REPORT "1+0+0 failed" SEVERITY failure;
+    tA <= "1111";
+    tB <= "0000";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '0' AND tQ = "1111" REPORT "15+0+0 failed" SEVERITY failure;
+    tA <= "1111";
+    tB <= "0001";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '1' AND tQ = "0000" REPORT "15+1+0 failed" SEVERITY failure;
 
-testing: process
+    tC <= '1';
+    tA <= "0000";
+    tB <= "0000";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '0' AND tQ = "0001" REPORT "0+0+1 failed" SEVERITY failure;
+    tA <= "0001";
+    tB <= "0000";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '0' AND tQ = "0010" REPORT "1+0+1 failed" SEVERITY failure;
+    tA <= "1111";
+    tB <= "0000";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '1' AND tQ = "0000" REPORT "15+0+1 failed" SEVERITY failure;
+    tA <= "1111";
+    tB <= "0001";
+    WAIT FOR 10 ns;
+    vypis;
+    ASSERT tCout = '1' AND tQ = "0001" REPORT "15+1+1 failed" SEVERITY failure;
+    REPORT "Test OK";
+    WAIT;
+  END PROCESS;
 
-function toString ( a: std_logic_vector) return string is
-  variable b : string (1 to a'length) := (others => NUL);
-  variable stri : integer := 1; 
-  begin
-    for i in a'range loop
-      b(stri) := std_logic'image(a((i)))(2);
-    stri := stri+1;
-    end loop;
-  return b;
-end function;
+  UUT : adder4B PORT MAP(tA, tB, tC, tQ, tCout);
 
-procedure vypis is
-  begin
-    report toString(tA) & " + " & toString(tB) & " + " & std_logic'image(tC) & 
-           " = " & std_logic'image(tCout) & toString(tQ);
-end procedure;
-
-    begin
-
-
-        tC <= '0';
-        tA <= "0000"; tB <= "0000"; wait for 10 ns; 
-        vypis;
-        assert tCout = '0' and tQ = "0000" report "0+0+0 failed" severity failure;
-        tA <= "0001"; tB <= "0000"; wait for 10 ns; 
-        vypis;
-        assert tCout = '0' and tQ = "0001" report "1+0+0 failed" severity failure;
-        tA <= "1111"; tB <= "0000"; wait for 10 ns; 
-        vypis;
-        assert tCout = '0' and tQ = "1111" report "15+0+0 failed" severity failure;
-        tA <= "1111"; tB <= "0001"; wait for 10 ns; 
-        vypis;
-        assert tCout = '1' and tQ = "0000" report "15+1+0 failed" severity failure;
-
-        tC <= '1';
-        tA <= "0000"; tB <= "0000"; wait for 10 ns; 
-        vypis;
-        assert tCout = '0' and tQ = "0001" report "0+0+1 failed" severity failure;
-        tA <= "0001"; tB <= "0000"; wait for 10 ns; 
-        vypis;
-        assert tCout = '0' and tQ = "0010" report "1+0+1 failed" severity failure;
-        tA <= "1111"; tB <= "0000"; wait for 10 ns; 
-        vypis;
-        assert tCout = '1' and tQ = "0000" report "15+0+1 failed" severity failure;
-        tA <= "1111"; tB <= "0001"; wait for 10 ns; 
-        vypis;
-        assert tCout = '1' and tQ = "0001" report "15+1+1 failed" severity failure;
-
-
-        report "Test OK";
-        wait;
-    end process;
-
-UUT: adder4B port map (tA,tB,tC,tQ,tCout);
-
-end bench;
+END bench;
