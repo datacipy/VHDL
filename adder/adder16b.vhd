dde2a78b5c2948b8e12f07b1a7bbae7b80d13ac6
Martin Maly <maly@maly.cz>
2020-05-22 12:04:07 +0200

    formatter

--- adder/adder16b.vhd
+++ adder/adder16b.vhd
@@ -1,32 +1,33 @@
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-entity adder16B is
- port (A, B: in std_logic_vector (15 downto 0);
- Cin: in std_logic;
- Q: out std_logic_vector (15 downto 0);
- Cout: out std_logic);
-end entity;
+ENTITY adder16B IS
+    PORT (
+        A, B : IN std_logic_vector (15 DOWNTO 0);
+        Cin : IN std_logic;
+        Q : OUT std_logic_vector (15 DOWNTO 0);
+        Cout : OUT std_logic);
+END ENTITY;
 
-architecture main of adder16B is
+ARCHITECTURE main OF adder16B IS
 
-    constant wide: integer := 16;
+    CONSTANT wide : INTEGER := 16;
 
-component fullAdder is
- port (
- A, B, Cin: in std_logic;
- Q, Cout: out std_logic
- );
-end component; 
+    COMPONENT fullAdder IS
+        PORT (
+            A, B, Cin : IN std_logic;
+            Q, Cout : OUT std_logic
+        );
+    END COMPONENT;
 
-signal C: std_logic_vector(wide downto 0);
+    SIGNAL C : std_logic_vector(wide DOWNTO 0);
 
-begin
-adders: for N in 0 to wide-1 generate
- myadder: fulladder port map (
- A(N),B(N), C(N), Q(N), C(N+1)
- );
-end generate;
-C(0) <= Cin;
-Cout <= C(wide);
-end architecture;
+BEGIN
+    adders : FOR N IN 0 TO wide - 1 GENERATE
+        myadder : fulladder PORT MAP(
+            A(N), B(N), C(N), Q(N), C(N + 1)
+        );
+    END GENERATE;
+    C(0) <= Cin;
+    Cout <= C(wide);
+END ARCHITECTURE;
