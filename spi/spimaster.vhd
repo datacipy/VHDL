dee712a7e6769fdedba175566b75078bc256323e
Martin Maly <maly@maly.cz>
2020-05-31 18:41:02 +0200

    spi

--- spi/spimaster.vhd
+++ spi/spimaster.vhd
@@ -15,7 +15,7 @@ ENTITY spiMaster IS
         cont : IN STD_LOGIC; --pokracovani prenosu
         TxD : IN STD_LOGIC_VECTOR(7 DOWNTO 0); --vstup dat
         miso : IN STD_LOGIC; --master in, slave out
-        sclk : BUFFER STD_LOGIC; --spi clock
+        sck : BUFFER STD_LOGIC; --spi clock
         mosi : OUT STD_LOGIC; --master out, slave in
         busy : OUT STD_LOGIC := '1'; --vysilac pracuje / data nejsou pripravena
         RxD : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --vystup dat
@@ -34,95 +34,98 @@ ARCHITECTURE main OF spiMaster IS
     SIGNAL cpolVectorize : std_logic_vector(0 TO 0);
 BEGIN
 
-    cpolVectorize(0) <= cpol; --hack
+    cpolVectorize(0) <= cpol; --hack kvuli aritmetice
     PROCESS (clock, reset)
     BEGIN
 
-        IF (reset = '1') THEN --reset system
-            busy <= '1'; --set busy signal
-            mosi <= 'Z'; --set master out to high impedance
-            RxD <= (OTHERS => '0'); --clear receive data port
-            state <= ready; --go to ready state when reset is exited
+        IF (reset = '1') THEN --reset
+            busy <= '1';
+            mosi <= 'Z';
+            RxD <= (OTHERS => '0');
+            state <= ready;
 
         ELSIF (rising_edge(clock)) THEN
             CASE state IS --state FSM
 
                 WHEN ready =>
-                    busy <= '0'; --clock out not busy signal
-                    mosi <= 'Z'; --set mosi output high impedance
-                    continue <= '0'; --clear continue flag
+                    busy <= '0';
+                    mosi <= 'Z';
+                    continue <= '0';
                     count <= 1;
 
-                    --user input to initiate transaction
+                    --zacina transakce
                     IF (cs = '1') THEN
-                        busy <= '1'; --set busy signal
-                        sclk <= cpol; --set spi clock polarity
-                        txrx <= NOT cpha; --set spi clock phase
-                        txBuffer <= TxD; --clock in data for transmit into buffer
-                        clkToggle <= 0; --initiate clock toggle counter
-                        lastBitRx <= 15 + to_integer(unsigned(cpolVectorize)); --set last rx data bit
-                        state <= execute; --proceed to execute state
-                        count <= CLKDIV;
+                        busy <= '1';
+                        sck <= cpol;
+                        txrx <= NOT cpha;
+                        txBuffer <= TxD;
+                        clkToggle <= 0;
+                        -- posledni zmena hodin, ve ktere se prijimaji data
+                        lastBitRx <= 15 + to_integer(unsigned(cpolVectorize));
+                        state <= execute;
+                        count <= CLKDIV; -- aby zmena probehla hned
                     ELSE
-                        state <= ready; --remain in ready state
+                        state <= ready;
                     END IF;
 
                 WHEN execute =>
-                    busy <= '1'; --set busy signal
+                    busy <= '1';
 
-                    --system clock to sclk ratio is met
+                    --pokud je delicka kmitoctu na nejvyssi hodnote
                     IF (count = CLKDIV) THEN
-                        count <= 1; --reset system-to-spi clock counter
-                        txrx <= NOT txrx; --switch transmit/receive indicator
+                        count <= 1; --nulujeme delicku
+                        txrx <= NOT txrx;
+
+                        --pocet prepnuti hodinoveho pulsu
                         IF (clkToggle = 17) THEN
-                            clkToggle <= 0; --reset spi clock toggles counter
+                            clkToggle <= 0; --uz jsme skoncili
                         ELSE
-                            clkToggle <= clkToggle + 1; --increment spi clock toggles counter
+                            clkToggle <= clkToggle + 1;
                         END IF;
 
-                        --spi clock toggle needed
+                        --je potreba zmenit SCK?
                         IF (clkToggle < 16) THEN
-                            sclk <= NOT sclk; --toggle spi clock
+                            sck <= NOT sck;
                         END IF;
 
-                        --receive spi clock toggle
+                        --prijem
                         IF (txrx = '0' AND clkToggle < lastBitRx + 1) THEN
-                            rxBuffer <= rxBuffer(6 DOWNTO 0) & miso; --shift in received bit
+                            rxBuffer <= rxBuffer(6 DOWNTO 0) & miso;
                         END IF;
 
-                        --transmit spi clock toggle
+                        --vysilani
                         IF (txrx = '1' AND clkToggle < lastBitRx) THEN
-                            mosi <= txBuffer(7); --clock out data bit
-                            txBuffer <= txBuffer(6 DOWNTO 0) & '0'; --shift data transmit buffer
+                            mosi <= txBuffer(7);
+                            txBuffer <= txBuffer(6 DOWNTO 0) & '0';
                         END IF;
 
-                        --last data receive, but continue
+                        --posledni bit, ale pokracuje se
                         IF (clkToggle = lastBitRx AND cont = '1') THEN
-                            txBuffer <= TxD; --reload transmit buffer
-                            clkToggle <= lastBitRx - 17; --reset spi clock toggle counter
-                            continue <= '1'; --set continue flag
+                            txBuffer <= TxD; --nacist novou hodnotu
+                            clkToggle <= lastBitRx - 17;
+                            continue <= '1';
                         END IF;
 
-                        --normal end of transaction, but continue
+                        --prenos byte skoncil, ale pokracuje se
                         IF (continue = '1') THEN
-                            continue <= '0'; --clear continue flag
-                            busy <= '0'; --clock out signal that first receive data is ready
-                            RxD <= rxBuffer; --clock out received data to output port    
+                            continue <= '0';
+                            busy <= '0'; --puls na vystupu busy dava signal pro dalsi prenos
+                            RxD <= rxBuffer;
                         END IF;
 
-                        --end of transaction
+                        --konec transakce?
                         IF ((clkToggle = 17) AND cont = '0') THEN
-                            busy <= '0'; --clock out not busy signal
-                            mosi <= 'Z'; --set mosi output high impedance
-                            RxD <= rxBuffer; --clock out received data to output port
-                            state <= ready; --return to ready state
-                        ELSE --not end of transaction
-                            state <= execute; --remain in execute state
+                            busy <= '0'; --busy zrusit
+                            mosi <= 'Z';
+                            RxD <= rxBuffer; --precteny byte
+                            state <= ready; --zpet do stavu ready
+                        ELSE --jeste neni konec?
+                            state <= execute; --zustavame ve stavu execute
                         END IF;
 
-                    ELSE --system clock to sclk ratio not met
-                        count <= count + 1; --increment counter
-                        state <= execute; --remain in execute state
+                    ELSE --delicka kmitoctu jeste nedopocitala k maximu?
+                        count <= count + 1; --zvysime jeji hodnotu
+                        state <= execute; --zustavame ve stavu execute
                     END IF;
 
             END CASE;
